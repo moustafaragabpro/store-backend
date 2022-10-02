f9c794f0dd08342387ef277ee083c87fe97b555a
moustafargbpro <moustafargbpro@outlook.com>
2022-10-02 21:11:45 +0200

    sql package

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,70 @@
-import { Router } from "express";
-
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import responses from '../helpers/responses.js';
+import conn from '../main.js';
+import userSchema from '../schemas/user.schema.js';
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {}); // TODO get all users
-userRouter.post("/", (req, res) => {}); // TODO create a new user
-userRouter.put("/", (req, res) => {}); // TODO update a user
-userRouter.delete("/", (req, res) => {}); // TODO delete a user
-userRouter.post("/login", (req, res) => {}); // TODO login user
+userRouter.get('/', (req, res) => {
+    const sql = 'SELECT * FROM users';
+    conn.query(sql, (err, result) => {
+        if (err) throw err;
+
+        responses.success(res, 'GET all users', result);
+    });
+}); // TODO get all users
+
+userRouter.post('/', async (req, res) => {
+    const { error, value } = userSchema.validate(req.body);
+    if (error) responses.badRequest(res, error.details[0].message);
+
+    const { name, email, password } = value;
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const sql = 'INSERT INTO users(name , email , password)  VALUES(?,?,?) ';
+
+    conn.query(sql, [name, email, hashedPassword], (err, result) => {
+        if (err) throw err;
+        else console.log('Inserted 1 row');
+    });
+
+    responses.success(res, 'User Created Succesfully', value);
+}); // TODO create a new user
+
+userRouter.put('/:id', (req, res) => {
+    const { id } = req.params;
+    let getUser = `SELECT * FROM users WHERE id='${id}'`;
+
+    conn.query(getUser, async (err, result) => {
+        if (err) throw err;
+
+        const { name, email, password } = req.body;
+        const sql =
+            'UPDATE users SET name=? , email=? , password=? WHERE id =?';
+
+        // console.log(result);
+        const match = await bcrypt.compare(password, result[0].password);
+
+        if (match) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            conn.query(
+                sql,
+                [name, email, hashedPassword, id],
+                (err, result) => {
+                    if (err) throw err;
+
+                    responses.success(
+                        res,
+                        'User updated successfully',
+                        result.message
+                    );
+                }
+            );
+        } else responses.badRequest(res, 'wrong Password ');
+    });
+}); // TODO update a user
+userRouter.delete('/', (req, res) => {}); // TODO delete a user
+userRouter.post('/login', (req, res) => {}); // TODO login user
 
 export default userRouter;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,54 @@
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import errorHandler from "./middlewares/errorHandller.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import mysql from 'mysql';
+import errorHandler from './middlewares/errorHandller.js';
+import userRouter from './controllers/user.controller.js';
+dotenv.config(); // load environment variables
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DB,
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('DB Connected');
+
+    conn.query('CREATE DATABASE IF NOT EXISTS store', (err, result) => {
+        if (err) throw err;
+        console.log('DB Created');
+    });
+
+    conn.query(
+        `CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(60),
+            email VARCHAR(255),
+            password VARCHAR(255)
+        )
+        `,
+        (err, result) => {
+            if (err) throw err;
+            console.log('Table created');
+        }
+    );
+});
+
+export default conn;
 
 const app = express(); // create express app
 
-dotenv.config(); // load environment variables
-
 app.use(express.json()); // to parse json data
 
-app.use(morgan("dev")); // to log requests
+app.use(morgan('dev')); // to log requests
 
 app.use(errorHandler); // to handle errors
+
+app.use('/users', userRouter);
 
 const PORT = process.env.PORT || 3000; // set port
 
